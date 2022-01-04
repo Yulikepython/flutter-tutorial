6ae18411c3b63a2dda789dc98290eddc3190b79e
yulikepython <admin@itc.tokyo>
2022-01-04 23:27:04 +0900

    first commit

---
+++ first_flutter_proj/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple[50]),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('my first app'),
+        ),
+        body: const Center(child: Text('Hello World')),
+      ),
+    );
+  }
+}
